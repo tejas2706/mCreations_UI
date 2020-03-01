755cc92fdac0a9db1971d98c352af232ae844bc5
tejas <Tejas1.Lad@ril.com>
2020-03-01 14:13:27 +0530

    Splash screen

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React,{ useEffect } from 'react';
 import Route from './route';
 import {Provider} from 'react-redux'
 import store from './redux/store'
+import SplashScreen from 'react-native-splash-screen'
 
 const App = () => {
+    useEffect(()=>{
+        SplashScreen.hide();
+    },[]);
     return (
         <Provider store={store}>
             <Route />
